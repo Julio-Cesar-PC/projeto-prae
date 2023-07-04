b2a70c6b369a1a0c235d62b69146f78f0afe1616
Julio Cesar Polmann Cuencas <juliocesarpolmann@gmail.com>
2023-07-04 18:54:34 -0300

    add admin middleware

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
             'password' => bcrypt('password'),
+            'admin' => true,
         ]);
 
         \App\Models\Category::factory()->create([
